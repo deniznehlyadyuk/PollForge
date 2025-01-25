0ea5118f73770c1b13b8bc6d7a6b81821dc1af20
Deniz Satır <deniznehlyadyuk@gmail.com>
2025-01-25 20:29:52 +0300

    update claim type of email

--- src/PollForge.Application/Users/Me/MeCommandHandler.cs
+++ src/PollForge.Application/Users/Me/MeCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using PollForge.Application.Abstractions.Messaging;
 using PollForge.SharedKernel;
@@ -10,7 +11,7 @@ internal sealed class MeCommandHandler(IHttpContextAccessor httpContextAccessor)
     public Task<Result<MeCommandResponse>> Handle(MeCommand request, CancellationToken cancellationToken)
     {
         var email = httpContextAccessor.HttpContext.User.Claims
-            .First(claim => claim.Type == JwtRegisteredClaimNames.Email).Value;
+            .First(claim => claim.Type == ClaimTypes.Email).Value;
         
         var fullName = httpContextAccessor.HttpContext.User.Claims
             .First(claim => claim.Type == JwtRegisteredClaimNames.Name).Value;
